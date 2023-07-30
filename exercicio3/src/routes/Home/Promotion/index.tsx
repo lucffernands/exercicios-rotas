b17fc05ffaa7ac89f5249277556ada66dc4dc78b
Luciano Fernandes <lucianoffernands@gmail.com>
2023-07-30 12:14:05 -0300

    Ex3

---
+++ exercicio3/src/routes/Home/Promotion/index.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import Card from "../../../components/Card";
+import Button from "../../../components/Button";
+
+export default function Promotion() {
+
+    return (
+        <main>
+            <section>
+                <div className="container">
+                    <div className="mt30 mb30">
+                        <Card title="Página de promoção" />
+                    </div>
+                    <div className="dflex">
+                        <Link to="/sub">
+                            <Button text="Quero participar" />
+                        </Link>
+                    </div>
+                </div>
+            </section>
+        </main>
+    );
+}
